5423748ba53cab8a207192d37b6ce2e6ba8f34e3
Boblord14 <falsevariable@gmail.com>
2026-06-23 14:16:56 -0400

    hw4 done

--- hw3/problem 4/Hopscotch.cpp
+++ hw3/problem 4/Hopscotch.cpp
@@ -6,6 +6,22 @@
 #include <cmath>
 using namespace std;
 
+int hopscotch(const vector<int> &numbers, const int len) {
+    int sum_3 = 0;
+    int sum_2 = 0;
+    int sum_skip = 0;
+    for (int i = 0; i < len; i++) {
+        int sum_current = sum_3 > sum_2 ? sum_3 + numbers[i] : sum_2 + numbers[i];
+        if (i == 1) {
+            sum_current = 0; // impossible to reach
+        }
+        sum_3 = sum_2;
+        sum_2 = sum_skip;
+        sum_skip = sum_current;
+    }
+    return max(sum_skip, sum_2);
+}
+
 int main () {
 
     int n;
@@ -19,8 +35,9 @@ int main () {
         squares[i] = x;
     }
 
+    int sum = hopscotch(squares, n);
 
-    cout << n << endl;
+    cout << sum << endl;
 
     return 0;
 }
